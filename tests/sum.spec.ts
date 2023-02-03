b3243993ad2febad499c02e4d471da0062c1cb56
lucasvtiradentes <lucasvtiradentes@gmail.com>
2023-02-03 15:26:13 -0300

    :bug: bugfix: update some basic testing (#30)
    See issues: 30

--- tests/sum.spec.ts
+++ tests/sum.spec.ts
@@ -2,6 +2,6 @@ it('should sum: 2 + 2', () => {
   expect(2 + 2).toBe(4);
 });
 
-it('should sum: 1 + 2', () => {
-  expect(1 + 2).toBe(3);
+it('should sum: 12 + 2', () => {
+  expect(12 + 2).toBe(14);
 });
