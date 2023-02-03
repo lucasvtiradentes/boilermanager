6cdac0d2d89da9414072989e67853ec67e22fc1c
lucasvtiradentes <lucasvtiradentes@gmail.com>
2023-02-03 18:26:43 -0300

    :bug: bugfix: add basic unit tests (#32)
    See issues: 32

--- tests/sum.spec.ts
+++ tests/sum.spec.ts
@@ -2,6 +2,6 @@ it('should sum: 2 + 2', () => {
   expect(2 + 2).toBe(4);
 });
 
-it('should sum: 12 + 2', () => {
-  expect(12 + 2).toBe(14);
+it('should sum: 12 + 3', () => {
+  expect(12 + 3).toBe(15);
 });
